532cd349f6385b51ec972ba7b9c3f587279b5fda
dbswlgp <46889729+dbswlgp@users.noreply.github.com>
2024-10-16 16:55:47 +0900

    [Silver I] Title: 카드 구매하기, Time: 112 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+price = list(map(int,input().split()))
+
+dp = [0] * (N+1)
+
+for i in range(1,N+1):
+  dp[i] = price[i-1]
+
+for i in range(2,N+1):
+  for j in range(1,i//2+1):
+    dp[i] = max(dp[i],dp[i-j]+dp[j])
+
+print(dp[N])
